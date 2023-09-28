be6263e442623131590dd5cb9d7befc6a824ca8d
tomowens08 <tomowens08@gmail.com>
2023-09-28 11:37:38 +0100

    Initial commit

---
+++ pap/data.js
@@ -0,0 +1,21 @@
+// JSON from json_encode($data)
+let json = [
+{"prod_id":00,"prod":[{"link":"","title":"Alcai Berry","price":"5.00","description":"Alcai Berry Supplement, comes in convenient pouch, many sizes available","img":"w.jpg"}]},
+{"prod_id":01,"prod":[{"link":"","title":"Raw Milk Powder","price":"15.00","description":"Raw Milk Powder reconstituted using whye and milk powder","img":"ai.png"}]},
+{"prod_id":02,"prod":[{"link":"","title":"Iron","price":"25.00","description":"Iron Supplement","img":"w.jpg"}]},
+{"prod_id":03,"prod":[{"link":"","title":"Vitamin D","price":"55.00","description":"Cod Liver Oil Extract","img":"ai.png"}]},
+{"prod_id":04,"prod":[{"link":"","title":"Cordyceps","price":"500.00","description":"Cordyceps Extract","img":"w.jpg"}]},
+{"prod_id":05,"prod":[{"link":"","title":"Vitamin C","price":"512.00","description":"Vitamin C","img":"ai.png"}]},
+{"prod_id":06,"prod":[{"link":"","title":"Calcium","price":"81.00","description":"Calcium","img":"w.jpg"}]},
+{"prod_id":07,"prod":[{"link":"","title":"Copper","price":"81.00","description":"Copper","img":"ai.png"}]},
+{"prod_id":08,"prod":[{"link":"","title":"Colodial Silver","price":"81.00","description":"Colodial Silver","img":"w.jpg"}]},
+{"prod_id":09,"prod":[{"link":"","title":"Iodine","price":"81.00","description":"Iodine","img":"ai.png"}]},
+{"prod_id":10,"prod":[{"link":"","title":"Creatine","price":"41.00","description":"Creatine","img":"w.jpg"}]},
+{"prod_id":10,"prod":[{"link":"","title":"Milk Thistle","price":"31.00","description":"Milk Thistle Herbal Remedy","img":"w.jpg"}]},
+{"prod_id":10,"prod":[{"link":"","title":"Quercetin","price":"45.00","description":"Quercetin with Vitamin C","img":"w.jpg"}]},
+{"prod_id":10,"prod":[{"link":"","title":"Bone Focus","price":"81.00","description":"Bone Strength Focus Blend","img":"w.jpg"}]},
+{"prod_id":10,"prod":[{"link":"","title":"Brain Focus","price":"81.00","description":"Brain Focus Blend","img":"w.jpg"}]},
+{"prod_id":10,"prod":[{"link":"","title":"Black Seed Oil","price":"81.00","description":"Black Seed Oil","img":"w.jpg"}]},
+{"prod_id":10,"prod":[{"link":"","title":"Magnesium","price":"35.00","description":"Magnesium Large","img":"w.jpg"}]},
+{"prod_id":10,"prod":[{"link":"","title":"Glucosomine","price":"35.00","description":"Glucosomine for energy","img":"w.jpg"}]}
+];
---
+++ pap/js/all.js
@@ -0,0 +1,14 @@
+// JSON from json_encode($data)
+let json = [
+{"prod_id":00,"prod":[{"link":"","title":"Micro SD","price":"5.00","description":"Micro SD 4GB Memory Card, comes with adapter for SD Size, for Android and iOS","img":"msd4"}]},
+{"prod_id":01,"prod":[{"link":"","title":"Micro SD 64GB","price":"15.00","description":"Micro SD 4GB Memory Card, comes with adapter for SD Size.","img":"msd4"}]},
+{"prod_id":02,"prod":[{"link":"","title":"Micro SD 32Gb","price":"25.00","description":"Micro SD","img":"msd4"}]},
+{"prod_id":03,"prod":[{"link":"","title":"Micro SD + SD Adapter","price":"55.00","description":"Micro SD","img":"msd4"}]},
+{"prod_id":04,"prod":[{"link":"","title":"Spy Camera for Door","price":"155.00","description":"Spy Camera for Door, rechargable Battery and lead included.","img":"msd4"}]},
+{"prod_id":05,"prod":[{"link":"","title":"Power Bank","price":"512.00","description":"Micro SD","img":"msd4"}]},
+{"prod_id":06,"prod":[{"link":"","title":"Bluetooth Speaker","price":"81.00","description":"Micro SD","img":"msd4"}]},
+{"prod_id":07,"prod":[{"link":"","title":"Tiny Little Hands","price":"81.00","description":"Micro SD","img":"msd4"}]},
+{"prod_id":08,"prod":[{"link":"","title":"Drop Shipped Health Supplies","price":"81.00","description":"Micro SD","img":"msd4"}]},
+{"prod_id":09,"prod":[{"link":"","title":"Crystal iPhone Case","price":"81.00","description":"Micro SD","img":"msd4"}]},
+{"prod_id":10,"prod":[{"link":"","title":"Rechargable Quick Charge iPhone USB Adapter and Lead.","price":"81.00","description":"Micro SD","img":"msd4"}]}
+];
